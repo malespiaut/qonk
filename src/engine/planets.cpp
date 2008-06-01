bbda484170022fef82b9de021941af6547283be1
thebohemian <thebohemian@917c0414-312c-0410-a5b5-8fb0c60c5ce6>
2008-06-01 22:07:31 +0000

    - fixed planet distance
    git-svn-id: https://svn.code.sf.net/p/qonk/code/trunk@142 917c0414-312c-0410-a5b5-8fb0c60c5ce6

--- src/engine/planets.cpp
+++ src/engine/planets.cpp
@@ -20,7 +20,7 @@ using namespace std;
 Planet::Planet() : nearestPlanet(false), owner(), mother()
 {
 	rotationSpeed = ( ( rand() % 2 ) * 2 - 1 ) * ( rand() % 60000 + 20000 );
-	rotationDistance = 0.05 + 0.4 * ( double )rand() / RAND_MAX;
+	rotationDistance = 0.05 + 0.2 * ( double )rand() / RAND_MAX;
 	rotationOffset = rand() % 10000000;
 	size = 4 + rand() % 4; // [4 ... 8)
 	isAMoon = false;
